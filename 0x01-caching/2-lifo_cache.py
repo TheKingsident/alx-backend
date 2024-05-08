26b0f753b37874fcfd1255fa4eee51e3e1eb8949
TheKingsident <kingsident@gmail.com>
2024-05-08 19:39:50 +0100

    Updated

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -6,13 +6,13 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ FIFOCache class
+    """ LIFOCache inherits from BaseCaching
     """
+
     def __init__(self):
-        """ Initialize
+        """ Initiliaze
         """
         super().__init__()
-        
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -20,17 +20,14 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) >= self.MAX_ITEMS:
             last_key = next(reversed(self.cache_data))
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
 
         self.cache_data[key] = item
-        
 
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
