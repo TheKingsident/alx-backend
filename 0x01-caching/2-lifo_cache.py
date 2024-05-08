2e786d00092f016247474038cff6807fccb72a50
TheKingsident <kingsident@gmail.com>
2024-05-08 19:38:02 +0100

    Updaste

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,7 +2,6 @@
 """ 2. LIFO caching
 """
 
-from queue import LifoQueue
 from base_caching import BaseCaching
 
 
@@ -13,7 +12,7 @@ class LIFOCache(BaseCaching):
         """ Initialize
         """
         super().__init__()
-        self.queue = LifoQueue()
+        
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -21,13 +20,13 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.queue.get()
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = next(reversed(self.cache_data))
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
 
         self.cache_data[key] = item
-        self.queue.put(key)
+        
 
     def get(self, key):
         """ Get an item by key
