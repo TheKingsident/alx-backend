f6009f83187a804c51a682ca60f265442ed53569
TheKingsident <kingsident@gmail.com>
2024-05-08 19:32:11 +0100

    Added files for task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 2. LIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ FIFOCache class
+    """
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = next(reversed(self.cache_data))
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
